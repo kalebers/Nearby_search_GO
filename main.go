5b96e5621c39fdd676e4f1ee36791e518134af7f
Kalebe Szlachta <39911310+kalebers@users.noreply.github.com>
2023-01-11 14:48:28 -0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,39 +1,65 @@
 package main
 
-// precisa-se usar a latitude e longitude do lugar para pegar recomendacoes nearby: -25.440660614399487, -49.27741271669351
+// precisa-se usar a latitude e longitude do lugar para pegar recomendacoes nearby:
+// to use the maps api I have to enable the feature in gcp with the key
 
-// to do: how to save the json request in a file?
+// to do: how to save the json request in a file? done
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+const (
+	apiKey     = ""
+	location   = ""     // latitude,longitude of location
+	radius     = "5000" // radius in meters
+	keyword    = "restaurant"
+	maxResults = 10
+)
+
+type Response struct {
+	Results []struct {
+		Name   string  `json:"name"`
+		Rating float64 `json:"rating"`
+	} `json:"results"`
+}
+
 func main() {
-	url := "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-25.440660614399487,-49.277412716693516&radius=10000&type=restaurant&keyword=rankby&key="
-	method := "GET"
+	// Build the URL for the API request
+	url := fmt.Sprintf("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s&radius=%s&keyword=%s&maxresults=%d&key=%s", location, radius, keyword, maxResults, apiKey)
 
-	client := &http.Client{}
-	req, err := http.NewRequest(method, url, nil)
-
+	// Make the API request
+	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic(err)
 	}
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer res.Body.Close()
+	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(res.Body)
+	// Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic(err)
 	}
 	fmt.Println(string(body))
 
-	//os.WriteFile("go/go_exercises"+".txt", []byte(url), 0644)
+	// Unmarshal the JSON into a Go data structure
+	var data Response
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		panic(err)
+	}
+
+	// Print the names and ratings of the top 10 restaurants
+	for i, result := range data.Results {
+		fmt.Printf("%d. %s - Rating: %.1f\n", i+1, result.Name, result.Rating)
+	}
+
+	// Save the data to a local file
+	err = ioutil.WriteFile("data.json", body, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
